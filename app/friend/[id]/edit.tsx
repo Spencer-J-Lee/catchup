b4dc261292c1af4c37d48463ccd855b7a6a718ce
Spencer Lee <23061968+Spencer-J-Lee@users.noreply.github.com>
2026-05-15 21:15:11 -0700

    Improve UX of notes field

--- app/friend/[id]/edit.tsx
+++ app/friend/[id]/edit.tsx
@@ -73,9 +73,8 @@ const EditFriendScreen = () => {
           value={generalNotes}
           onChangeText={setGeneralNotes}
           multiline
-          numberOfLines={4}
           textAlignVertical="top"
-          className="h-24"
+          className="min-h-24 max-h-40"
         />
         <CadencePicker value={cadence} onChange={setCadence} />
         <Button
